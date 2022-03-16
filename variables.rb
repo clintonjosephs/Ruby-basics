76a99150f000742d987b7f6d6bfa9c749972b09a
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-16 15:57:11 +0100

    declaring and reading variables in ruby

---
+++ variables.rb
@@ -0,0 +1,10 @@
+character_name = "John";
+character_age = "35";
+
+puts ("There once was a man named " + character_name);
+puts ("he is " + character_age + " years old.");
+
+character_name = "Tom";
+
+puts ("He really liked the name " + character_name);
+puts ("but didnt like being " + character_age );
